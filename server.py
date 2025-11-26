fd363dac9e3505b37b48bae68541d2a263690621
amirhosein keivani <amirhosein8292@gmail.com>
2025-11-26 21:59:25 +0330

    second

--- server.py
+++ server.py
@@ -1,36 +1,39 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, send_file
 import cv2
 import numpy as np
+import io
 
 app = Flask(__name__)
 
-# Haar Cascade
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 @app.route("/detect", methods=["POST"])
 def detect():
-    # دریافت تصویر
+    if 'image' not in request.files:
+        return "No image provided", 400
+
     file = request.files['image']
     img_bytes = file.read()
     
-    # تبدیل به آرایه NumPy
     nparr = np.frombuffer(img_bytes, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    # پردازش
+    if img is None:
+        return "Invalid image", 400
+
     faces = face_cascade.detectMultiScale(img, 1.3, 4)
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    # بازگشت تصویر به Base64
+    # ذخیره در حافظه و ارسال
     _, buffer = cv2.imencode('.jpg', img)
-    encoded_img = np.array(buffer).tobytes()
+    io_buf = io.BytesIO(buffer)
 
-    return encoded_img
+    return send_file(io_buf, mimetype='image/jpeg')
 
 @app.route("/", methods=["GET"])
 def home():
-    return "Face detection API is running."
+    return "Cloud Face Detection API is running."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
